a12950cd7479b5caa0edd877b998490128061447
Rafael D <69222919+Rafael-Dur@users.noreply.github.com>
2020-10-13 12:21:32 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -17,7 +17,7 @@ namespace CompAndDel
             //p.SavePicture(pic,@"matrix2.jpg");
 
             /*
-            La idea es que cuando pase por el FilterSaving se guarde la imagen con el filto aplicado de donde viene.
+            La idea es que cuando pase por el FilterSaving se guarde la imagen con el filtro aplicado de donde viene.
             Le paso la foto al pipe grayScale que lo va a pasar por FilterGrayScale y de ahi a un pipe que se llama saveImage
             creado para este ejercicio, este pipe le aplica el filto que guarda la imagen. De ahi se va al pipe fork donde decide
             dependiendo de si tiene cara o no si va al twitterfilter o al negative.
